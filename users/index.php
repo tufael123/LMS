d45909ed6b3d27509d4046c688a3156a94d5dd49
Tufael Ahmed Tamim <tufael.contact@gmail.com>
2024-11-17 12:19:26 +0600

    Add files via upload

---
+++ users/index.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+if(isset($_SESSION['email'])){
+	$id = $_SESSION['id'];
+}
+else{
+	header('location: ../login.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Logged in</h1>
+    <a href="logout.php">logout</a>
+</body>
+</html>
